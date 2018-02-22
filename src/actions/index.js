e91c935564ef7c7057f03aabceedbc7d1764247a
system_user <alec.fekete@gmail.com>
2018-02-22 07:22:27 -0600

    comment out todo actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,26 +1,3 @@
-let nextTodoId = 0
-export const addTodo = text => {
-    return {
-        type: 'ADD_TODO',
-        id: nextTodoId++,
-        text
-    }
-}
-
-export const setVisibilityFilter = filter => {
-    return {
-        type: 'SET_VISIBILITY_FILTER',
-        filter
-    }
-}
-
-export const toggleTodo = id => {
-    return {
-        type: 'TOGGLE_TODO',
-        id
-    }
-}
-
 export const setStats = stats => {
     return {
         type: 'SET_STATS',
@@ -41,3 +18,26 @@ export const setClass = chosenClass => {
         chosenClass
     }
 }
+
+// let nextTodoId = 0
+// export const addTodo = text => {
+//     return {
+//         type: 'ADD_TODO',
+//         id: nextTodoId++,
+//         text
+//     }
+// }
+
+// export const setVisibilityFilter = filter => {
+//     return {
+//         type: 'SET_VISIBILITY_FILTER',
+//         filter
+//     }
+// }
+
+// export const toggleTodo = id => {
+//     return {
+//         type: 'TOGGLE_TODO',
+//         id
+//     }
+// }
